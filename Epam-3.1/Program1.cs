4cd9bc5eff1edc35193c4e530f1ab5685fc5063b
Cyb3rKotl3Ta <71874363+Cyb3rKotl3Ta@users.noreply.github.com>
2020-11-13 01:29:15 +0200

    Add files via upload

---
+++ Epam-3.1/Program1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Dynamic;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Epam_3._1
+{
+   
+   public class Rectangle
+    {
+        private double sideA { get; set; }
+        private double sideB { get; set; }
+        public double getsideA()
+        {
+            Console.WriteLine("Enter num1: ");
+            sideA = double.Parse(Console.ReadLine());
+            return sideA;
+        }
+        public double getsideB()
+        {
+            Console.WriteLine("Enter num2: ");
+            sideB = double.Parse(Console.ReadLine());
+            return sideB;
+        }
+        public double getper()
+        {
+            double per;
+            per = (getsideA() + getsideB()) * 2;
+            return per;
+        }
+        public double getpl()
+        {
+            double pl;
+            pl = getsideA() * getsideB();
+            return pl;
+        }
+
+    }
+
+    class Prog1
+    {
+
+        static void Main(string[] args)
+        {
+            Rectangle per = new Rectangle();
+            Rectangle pl = new Rectangle();
+            Console.WriteLine("Perimetr is: " + per.getper());
+            Console.WriteLine("Plo4a is: " + pl.getpl());
+
+
+        }
+
+    }
+
+}
+
+
+
