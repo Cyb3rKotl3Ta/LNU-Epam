e0b334b162b19ead08661b43b61d4cbc03e2d802
Cyb3rKotl3Ta <71874363+Cyb3rKotl3Ta@users.noreply.github.com>
2020-11-19 22:24:54 +0200

    Add files via upload

---
+++ Epam-6.1/Epam-6.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Epam_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            double[] myArray = new double [6] { 2, 3, 4, 5, 6, 187 };
+            double[] myArray1 = new double [6];
+            
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray1[i] = Math.Pow( myArray[i] , 3 );
+                Console.WriteLine(myArray1[i]);
+            }
+        }
+    }
+}
