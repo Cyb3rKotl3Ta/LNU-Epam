550613b8faa268d3467a52adbc4b25a496e789f8
Cyb3rKotl3Ta <71874363+Cyb3rKotl3Ta@users.noreply.github.com>
2020-11-19 22:26:10 +0200

    Add files via upload

---
+++ Epam-6.2/Epam-6.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Epam_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            double[] myArray =  { 12, 23, 14, 213423, 21, 1234, 23 };
+           
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] % 2 == 0)
+                {
+                    Console.WriteLine(myArray[i]);
+                }
+            }
+            
+        }
+    }
+}
+
+//Console.WriteLine(myArray.Where(i => i % 2 == 0));
---
+++ Epam-6.3/Epam-6.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Epam_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            double[] myArray = { 12, 23, 14, 213423, 21, 1234, 23 };
+            double[] myArray1 = { 12, 23, 14, 213423, 21, 1234, 24 };
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] == myArray1[i])
+                {
+                    //Console.WriteLine("Equival");
+                }
+                else
+                {
+                    ///Console.WriteLine("Not Equival");
+                }
+            }
+            if(myArray == myArray1)
+            {
+                Console.WriteLine("Are equival");
+            }
+            else
+            {
+                Console.WriteLine("Are not equival");
+            }
+        }
+    }
+}
