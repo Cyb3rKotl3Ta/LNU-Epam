db02a57257cc9848abb768ba6e2fe9b314622cdc
Cyb3rKotl3Ta <71874363+Cyb3rKotl3Ta@users.noreply.github.com>
2020-12-14 14:33:05 +0200

    Add files via upload

---
+++ Epam-9.1/Epam-9.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Epam_9._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            Console.WriteLine("Введiть повну або часткову назву директорii");
+            path = Console.ReadLine();
+            try
+            {
+                string[] dirs = Directory.GetFiles(@"D:\Epam", $"{path}*", SearchOption.AllDirectories);
+                string a = $"{dirs[0]}";
+                dirs = Directory.GetDirectories(@a, $"", SearchOption.AllDirectories);
+                foreach (string dir in dirs)
+                {
+                    Console.WriteLine(dir);
+                }
+                string[] files = Directory.GetFiles(@a, $"{path}*", SearchOption.AllDirectories);
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
